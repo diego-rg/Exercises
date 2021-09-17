b549b2767f392365aabef35a109f9c516a4b38b6
Diego <76407123+diego-rg@users.noreply.github.com>
2021-09-17 12:57:58 +0200

    finished

---
+++ async-promises/app.js
@@ -0,0 +1,51 @@
+//Rainbow Transition without promises:
+
+// const delayedColorChange = (newColor, delay, doNext) => {
+//     setTimeout(() => {
+//         document.body.style.backgroundColor = newColor;
+//         doNext && doNext();
+//     }, delay)
+// }
+
+// delayedColorChange('red', 1000, () => {
+//     delayedColorChange('orange', 1000, () => {
+//         delayedColorChange('yellow', 1000, () => {
+//             delayedColorChange('green', 1000, () => {
+//                 delayedColorChange('blue', 1000, () => {
+//                     delayedColorChange('indigo', 1000, () => {
+//                         delayedColorChange('violet', 1000, () => {
+
+//                         })
+//                     })
+//                 })
+//             })
+//         })
+//     })
+// });
+
+
+// --------------------------------------------------------------------------------------------------
+
+
+//Rainbow Transition with async/await promises:
+
+const delayedColorChange = (color, delay) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            document.body.style.backgroundColor = color;
+            resolve();
+        }, delay)
+    })
+}
+
+async function rainbow () {
+    await delayedColorChange('red', 1000)
+    await delayedColorChange('orange', 1000)
+    await delayedColorChange('yellow', 1000)
+    await delayedColorChange('green', 1000)
+    await delayedColorChange('blue', 1000)
+    await delayedColorChange('indigo', 1000)
+    await delayedColorChange('violet', 1000)
+}
+
+rainbow();
