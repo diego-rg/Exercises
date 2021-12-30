f43cb5dad084ec83a0449d63ef5d0af16c12f80a
Diego <76407123+diego-rg@users.noreply.github.com>
2021-12-30 14:36:55 +0100

    Add files via upload

---
+++ express-flash/index.js
@@ -0,0 +1,133 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const mongoose = require('mongoose');
+const methodOverride = require("method-override");//Instalamos e requerimos Method Override para crear metodo PUT e DELETE para editar
+app.use(methodOverride("_method"));
+const session = require("express-session");
+const flash = require("connect-flash");//Todos as req teñen .flash() para crear mensaxes flash (mensaxes temporales creados despois dunha reqquest)
+
+app.use(session({ secret: "secretSecret", resave: false, saveUninitialized: false }));
+app.use(flash());
+
+const categories = ['fruit', 'vegetable'];//Mellor afcelo coma en nono, sacando valor do array do schema? Así non actualiza...
+
+const Product = require("./models/product.js");
+const Farm = require("./models/farm");
+
+mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("MONGO CONNECTION OPEN!")
+    })
+    .catch(err => {
+        console.log("MONGO CONNECTION ERROR!")
+        console.log(err)
+    })
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }))//para usar middeware para post
+
+//Uso de middleware para pasar os messages de flash
+app.use((req, res, next) => {
+    res.locals.messages = req.flash("success");
+    next();
+})
+
+//FARM routes
+app.get("/farms", async (req, res) =>{
+    const farms = await Farm.find({})
+    res.render("farms/index", { farms }); //con messages: req.flash("success") });//Punto onde se mostra o flash. Hai que añadilo no template correspondente de ejs. Non fai falta se usas o middle
+})
+
+app.get("/farms/new", (req, res) => {
+    res.render("farms/new")
+})
+
+app.post("/farms", async (req, res) => {
+    const newFarm = new Farm(req.body);
+    await newFarm.save();
+    console.log(newFarm);
+    req.flash("success", "New farm saved!");//Punto no que se crea o flash
+    res.redirect("farms")
+})
+
+//Para crear producto asociado a granja:
+app.get("/farms/:id/products/new", async (req, res) => {
+    const { id } = req.params;
+    const farm = await Farm.findById(id);
+    res.render("products/new", { categories, farm })
+})
+
+app.post("/farms/:id/products", async (req, res) => {
+    const { id } = req.params;
+    const farm = await Farm.findById(id);
+    const { name, price, category } = req.body;
+    const product = new Product({ name, price, category });
+    farm.products.push(product);
+    product.farm = farm;
+    await farm.save();
+    await product.save();
+    res.redirect(`farms/${farm._id}`)
+})
+
+//ID despois de NEW senón da erro
+app.get("/farms/:id", async (req, res) =>{
+    const { id } = req.params;
+    const farm = await Farm.findById(id).populate("products");//Uso de populate para pasar todos os datos de product
+    res.render("farms/show", { farm })
+})
+
+app.delete("/farms/:id", async (req, res) => {
+    const farm = await Farm.findByIdAndDelete(req.params.id);
+    res.redirect("farms");
+})
+
+//PRODUCT REST routes
+app.get("/products", async (req, res) =>{
+    const products = await Product.find({})
+    res.render("products/index", { products });
+})
+
+//Novo producto: composto por Crear e Enviar
+//Crear: non async xa que non hai q esperar a nada
+app.get("/products/new", (req, res) => {
+    res.render("products/new")
+})
+
+//Enviar a db. Redirigir siempre para evitar quedar nun bucle que cree o producto varias veces
+app.post("/products", async (req, res) => {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    console.log(newProduct);
+    res.redirect(`/products/${newProduct._id}`)
+})
+
+app.get("/products/:id", async (req, res) =>{
+    const { id } = req.params;
+    const product = await Product.findById(id).populate("farm", "name");
+    res.render("products/show", { product })
+})
+
+//Editar
+app.get("/products/:id/edit", async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/edit', { product })
+})
+
+app.put("/products/:id", async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true });
+    res.redirect(`/products/${product._id}`);
+})
+
+app.delete("/products/:id", async (req, res) => {
+    const { id } = req.params;
+    const deleteProduct = await Product.findByIdAndDelete(id);
+    res.redirect("/products");
+})
+
+app.listen(3000, () =>{
+    console.log("Listening on port 3000");
+})
